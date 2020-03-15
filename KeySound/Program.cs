b65dc32b6a8fb244004e657efecf9dfa76ed7615
Gusein Atamoglanov <gusejnaga_a@holdingbp.ru>
2020-03-15 15:45:19 +0400

    Random sounds added

--- KeySound/Program.cs
+++ KeySound/Program.cs
@@ -6,27 +6,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using KeySound.Models;
+using System.Runtime.InteropServices;
 
 namespace KeySound {
     class Program {
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         static void Main(string[] args) {
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
             KeyboardHooker.KeyUp += KeyboardHooker_KeyUp;
             KeyboardHooker.KeyDown += KeyboardHooker_KeyDown;
             KeyboardHooker keyboardHooker = new KeyboardHooker();
         }
 
         private static void KeyboardHooker_KeyDown(Keys key) {
-            Console.WriteLine(key.ToString());
             SoundPlayer soundPlayer = new SoundPlayer();
             switch(key) {
+                case Keys.Enter:
+                    soundPlayer.SoundLocation = "Sounds/Enter-key.wav";
+                    break;
                 case Keys.Back:
-                    soundPlayer.SoundLocation = "Sounds/backspace.wav";
+                    soundPlayer.SoundLocation = "Sounds/Backspace-key.wav";
                     break;
                 case Keys.Space:
-                    soundPlayer.SoundLocation = "Sounds/space.wav";
+                    soundPlayer.SoundLocation = "Sounds/Space-key.wav";
                     break;
                 default:
-                    soundPlayer.SoundLocation = "Sounds/a.wav";
+                    var random = new Random();
+                    soundPlayer.SoundLocation = $"Sounds/{random.Next(0, 6)}-key.wav";
                     break;
 
             }
